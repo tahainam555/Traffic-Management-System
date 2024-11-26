4230a5364d57f5a069d9984fa693ef95e8984996
tahainam555 <tahainam879@gmail.com>
2024-11-27 01:12:35 +0500

    implementing priority queue for traffic signals

--- main.cpp
+++ main.cpp
@@ -29,24 +29,45 @@ int main(){
         i++;
     }
     file2.close();
+
+    file.open("data/traffic_signals.csv");
+    num=0;
+    while(getline(file, str)){
+        num++;
+    }
+    cout << num << endl;
+    file.close();
+    priorityQueue pq(num);
+    file2.open("data/traffic_signals.csv");
+    i=0;
+    while(getline(file2, str)){
+        if(i>0){    
+            char intersection = str[0];
+            int time = stoi(str.substr(2));
+            pq.enqueue(time, intersection);
+        }
+        i++;
+    }
     
     
     char ch;
     cout << "=============WELCOME TO THE ROAD NETWORK=============" << endl;
     cout << "1: DISPLAY CITY ROAD NETWORK" << endl;
     cout << "2: FIND SHORTEST PATH" << endl;
-    cout << "3: EXIT" << endl;
+    cout << "3: TRAFFIC SIGNAL STATUS" << endl;
     cout << "====================================================" << endl;
     cout << "Enter your choice: ";
     cin >> ch;
     if(ch == '1'){
+        cout << "=================CITY ROAD NETWORK==================" << endl;
         g.print();
     }
     else if(ch == '2'){
         
     }
     else if(ch == '3'){
-        return 0;
+        cout << "=================TRAFFIC SIGNAL STATUS==================" << endl;
+        pq.print();
     }
     else{
         cout << "Invalid choice" << endl;
--- taha.h
+++ taha.h
@@ -5,6 +5,8 @@
 #include<string>
 using namespace std;
 
+//================================================================================================
+
 int vertexHash(char ch){
     return ch - 'A';
 }
@@ -63,6 +65,93 @@ class Graph{
             }
         }
 };
+
+//================================================================================================
+
+
+class priorityQueueNode{
+    public:
+        char intersection;
+        int time;
+};
+
+class priorityQueue{
+    priorityQueueNode *data;
+    int size;
+    int capacity;
+    public:
+        priorityQueue(int num){
+            data = new priorityQueueNode[num+1];
+            size = 1;
+            capacity = num;
+        }
+
+        void enqueue(int time, char intersection){
+            if(size == capacity){
+                cout << "Queue is full" << endl;
+                return;
+            }
+            data[size].time = time;
+            data[size].intersection = intersection;
+            int i = size;
+            size++;
+            while(i > 1){
+                int parent = i/2;
+                if(data[parent].time > data[i].time){
+                    priorityQueueNode temp = data[parent];
+                    data[parent] = data[i];
+                    data[i] = temp;
+                    i = parent;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+
+        void dequeue(){
+            if(size == 1){
+                cout << "Queue is empty" << endl;
+                return;
+            }
+            data[1] = data[size-1];
+            size--;
+            int i = 1;
+            while(i < size){
+                int left = 2*i;
+                int right = 2*i + 1;
+                int minIndex = i;
+                if(left < size && data[left].time < data[minIndex].time){
+                    minIndex = left;
+                }
+                if(right < size && data[right].time < data[minIndex].time){
+                    minIndex = right;
+                }
+                if(minIndex == i){
+                    break;
+                }
+                priorityQueueNode temp = data[i];
+                data[i] = data[minIndex];
+                data[minIndex] = temp;
+                i = minIndex;
+            }
+        }
+
+        void displayQueue(){
+            while(size > 1){
+                cout << data[1].intersection << " Green Time " << data[1].time << "\n";
+                dequeue();
+            }
+        }
+
+        void print(){
+            for(int i = 1; i < size; i++){
+               cout << data[i].intersection << " Green Time " << data[i].time << endl;
+            }
+        }
+};
+
+//================================================================================================
 
 
 #endif
